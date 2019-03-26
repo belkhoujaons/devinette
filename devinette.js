37c0a9f8dd2b1e372973a55091224a0793645bed
belkhoujaons <belkhoujaons@gmail.com>
2019-03-26 20:21:37 +0100

    code js

---
+++ devinette.js
@@ -0,0 +1,18 @@
+function number () {
+var x= number ;
+var i=0 ;
+var random = Math.random() * 101;
+do {
+if (random==x) 
+{ (result="YES!!! You made it BOSS ") && (i=8)   ;}
+ else if (random<x)
+ {(result="you're close! just a little lower") && (i++) ; }
+  else if(random>x)
+ {(result="you're close ! just a little higher") && (i++) ; 
+ while (i!=8) ;
+
+ 
+ 
+
+}
+}
