cc8e7e530ee9915bd4e1e6c25c7a06a9bf102cd7
belkhoujaons <belkhoujaons@gmail.com>
2019-03-26 23:43:10 +0100

    55

--- devinette.js
+++ devinette.js
@@ -1,18 +1,18 @@
-alerte="YES ! you made it Boss!"
+alert="Yes you made it BOSS" ;
 function number () {
     var x = document.getElementById("num");
 var i=0 ;
+var result ;
 var random = Math.random() * 101;
-do {
-if (random==x.innerHTML) 
-{ (alerte="YES ! you made it Boss!") && (i=8)   ;}
- else if (random<x.innerHTML)
- {(alerte="you're close! just a little lower") && (i++) ; }
-  else if(random>x.innerHTML)
- {(alerte="you're close! just a little higher") && (i++) ;} }
- while ((i!=8)) ;
+if (random==x) 
+{ (alert="Yes you made it BOSS") && (i=8)   ;}
+ else if (random<x)
+ {(alert="you're close ! just a little lower") && (i++) ; }
+  else if(random>x)
+ {(alert="you're close ! just a little higher") && (i++) ;} }
+ 
 
  
  
 
-}
+
