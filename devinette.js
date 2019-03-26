676f8a9111fde4134e74067a8367c1dbb12247ce
belkhoujaons <belkhoujaons@gmail.com>
2019-03-26 22:24:21 +0100

    j

--- devinette.js
+++ devinette.js
@@ -1,15 +1,16 @@
+
 function number () {
-var x= number ;
+    var x = document.getElementById("num").value;
 var i=0 ;
 var random = Math.random() * 101;
 do {
 if (random==x) 
-{ (result="YES!!! You made it BOSS ") && (i=8)   ;}
+{ (alerte="YES ! you made it Boss!") && (i=8)   ;}
  else if (random<x)
- {(result="you're close! just a little lower") && (i++) ; }
+ {(alerte="you're close! just a little lower") && (i++) ; }
   else if(random>x)
- {(result="you're close ! just a little higher") && (i++) ;} }
- while (i!=8) ;
+ {(alerte="you're close! just a little higher") && (i++) ;} }
+ while ((i!=8)) ;
 
  
  
